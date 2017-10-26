03fd1f08b16e2f1f420e99511d9acaf6c14db858
Matheus Graciano <mathgunz@gmail.com>
2017-10-25 22:16:48 -0200

    tela nova

--- controleretorno/src/main/java/br/com/doctorfeet/controleretorno/controller/ClienteController.java
+++ controleretorno/src/main/java/br/com/doctorfeet/controleretorno/controller/ClienteController.java
@@ -3,7 +3,9 @@ package br.com.doctorfeet.controleretorno.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import br.com.doctorfeet.controleretorno.model.Cliente;
 import br.com.doctorfeet.controleretorno.repository.ClienteRepository;
@@ -21,6 +23,16 @@ public class ClienteController {
 		model.addAttribute("clientes", clientes);
 
 		return "cadastro-cliente";
+	}
+	
+	@RequestMapping(value="/cliente/edit/{id}", method = RequestMethod.GET)
+	public String editCliente(@PathVariable("id") Long id, Model model){
+		
+		Cliente cliente = this.repository.findOne(id);
+		
+		model.addAttribute("editarCliente", cliente);
+		
+		return "editar-cliente";
 	}
 
 }
--- controleretorno/src/main/java/br/com/doctorfeet/controleretorno/model/Cliente.java
+++ controleretorno/src/main/java/br/com/doctorfeet/controleretorno/model/Cliente.java
@@ -21,7 +21,7 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Integer id;
+    private Long id;
 
     private String nome;
 
@@ -46,15 +46,15 @@ public class Cliente {
 
     public Cliente(){}
     
-    public Cliente(Integer id) {
+    public Cliente(Long id) {
         this.id = id;
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
